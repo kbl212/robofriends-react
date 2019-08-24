fec7b5317d382affded98dc56ebea86c4f8f3ba9
Kyle Lauritzen <kyle.lauritzen@sophos.com>
2019-08-24 18:14:14 -0400

    initial commit. app works, displays CardList, list is searchable

---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Kyle Lauritzen',
+    username: 'EsteGuey',
+    email: 'kyle.lauritzen@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Dina Lauritzen',
+    username: 'EstaGuey',
+    email: 'lauritzendina@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Someone Else',
+    username: 'SomeOne',
+    email: 'sum1@gmail.com'
+  }
+]
