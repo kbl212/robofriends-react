ad0905fa9690ccbc8c3018048d0469e7733c3889
Kyle Lauritzen <kyle.lauritzen@sophos.com>
2019-08-25 00:22:55 -0400

    connected app to Redux. added actions and reducers.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    searchField: state.searchField
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+}
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       robots: [],
-      searchfield: ''
     }
-    console.log('constructing');
   }
 
   componentDidMount() {
-    console.log('didMount');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => {
       return response.json();
@@ -24,21 +36,19 @@ class App extends React.Component {
       this.setState({ robots: users})
     });
   }
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-  }
 
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
     return !robots.length ?
       <h1>Loading</h1> :
       (
       <div className='tc'>
         <h1>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           <ErrorBoundary>
             <CardList robots={filteredRobots} />
@@ -49,4 +59,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
