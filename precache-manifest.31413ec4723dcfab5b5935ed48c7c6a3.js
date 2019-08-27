bac49b9f75093faf2fd258a203a4d00562d6d91c
Kyle Lauritzen <kyle.lauritzen@sophos.com>
2019-08-26 22:18:33 -0400

    Updates

---
+++ precache-manifest.31413ec4723dcfab5b5935ed48c7c6a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24c9d1c095f1054170cd1524644e8ec",
+    "url": "/robofriends-react/index.html"
+  },
+  {
+    "revision": "eecae423b8919bf1693b",
+    "url": "/robofriends-react/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ac5a4dba9d3a5a5910ca",
+    "url": "/robofriends-react/static/css/main.8cbfbb12.chunk.css"
+  },
+  {
+    "revision": "eecae423b8919bf1693b",
+    "url": "/robofriends-react/static/js/2.2d4747dd.chunk.js"
+  },
+  {
+    "revision": "ac5a4dba9d3a5a5910ca",
+    "url": "/robofriends-react/static/js/main.9e53ceb6.chunk.js"
+  },
+  {
+    "revision": "1b58c69c69d4d6877d89",
+    "url": "/robofriends-react/static/js/runtime~main.c664e1be.js"
+  }
+]);
